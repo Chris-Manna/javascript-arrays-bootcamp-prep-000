f99aeaa8c8af6ab641eda243cdc26f5985d00adc
Christopher Manna <cmanna285@gmail.com>
2018-08-09 19:39:25 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,8 +4,6 @@ function addElementToBeginningOfArray(arr, element) {
   return [element, ...arr]
 }
 function destructivelyAddElementToBeginningOfArray(arr, element) {
-  console.log(`helllo ${arr}`)
   arr.unshift(element)
-  console.log(`hello second time, ${arr}`)
-  return arr
+  // return arr
 }
